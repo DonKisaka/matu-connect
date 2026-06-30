8e06a4204edea3af9dd5d3f9aa96b5a035a1a5d1
DonaldKisaka <davidkisaka4@gmail.com>
2026-06-30 11:02:20 +0300

    Initial commit

---
+++ matuconnect/src/main/java/com/matuconnect/MatuconnectApplication.java
@@ -0,0 +1,13 @@
+package com.matuconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatuconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatuconnectApplication.class, args);
+	}
+
+}
