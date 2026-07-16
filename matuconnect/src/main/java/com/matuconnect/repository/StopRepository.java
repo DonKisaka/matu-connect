53706eab87652b7546c7229ca80aa71968cbafe6
DonaldKisaka <davidkisaka4@gmail.com>
2026-07-16 10:44:30 +0300

    Added stop by name method

--- matuconnect/src/main/java/com/matuconnect/repository/StopRepository.java
+++ matuconnect/src/main/java/com/matuconnect/repository/StopRepository.java
@@ -5,6 +5,10 @@ import com.matuconnect.model.Stop;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface StopRepository extends JpaRepository<Stop, String> {
+
+    List<Stop> findByStopNameContainingIgnoreCase(String partialName);
 }
